d69e7ecd5b26d0451773639717dd9a4d7e3303e3
Alexander Zaytsev <zaytsev126@gmail.com>
2018-04-04 06:00:21 +0300

    Fetch clients

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+const URL = 'https://raw.githubusercontent.com/shoonia/incode/master/__task__/clients.json';
+
 class App extends Component {
+  state = {
+    clients: []
+  }
+
+  componentDidMount () {
+    fetch(URL).then(res => res.json())
+      .then(clients => {
+        this.setState({ clients });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container mt-3">
+          <div className="row">
+            <div className="col-4">
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col-8">block</div>
+          </div>
       </div>
     );
   }
